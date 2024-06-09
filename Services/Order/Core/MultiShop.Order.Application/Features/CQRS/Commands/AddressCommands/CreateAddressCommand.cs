2efc313bcad92063c376a2748e816488589db946
AK <ali.kuru6@hotmail.com>
2024-06-09 15:15:23 +0300

    -

--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/CreateAddressCommand.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/CreateAddressCommand.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultiShop.Order.Domain.Entities
+namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands
 {
-    public class Address
+    public class CreateAddressCommand
     {
-        public int AddressId { get; set; }
         public string UserId { get; set; }
         public string District { get; set; }
         public string City { get; set; }
