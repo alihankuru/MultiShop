09ae678cb014a6b0730b835cd553401142e5c82d
AK <ali.kuru6@hotmail.com>
2024-06-21 18:05:14 +0300

    -

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompanyController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCompanyDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCompanyController : ControllerBase
+    {
+        private readonly ICargoCompanyService _cargoCompanyService;
+
+        public CargoCompanyController(ICargoCompanyService cargoCompanyService)
+        {
+            _cargoCompanyService = cargoCompanyService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCompanyList()
+        {
+            var values=_cargoCompanyService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCompany(CreateCargoCompanyDto createCargoCompanyDto)
+        {
+            CargoCompany cargoCompany = new CargoCompany()
+            {
+                CargoCompanyName=createCargoCompanyDto.CargoCompanyName,
+            };
+            _cargoCompanyService.TInsert(cargoCompany);
+            return Ok("Kargo Şirketi başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoCompany(int id)
+        {
+            _cargoCompanyService.TDelete(id);
+            return Ok("Kargo Şirketi Başarıyla Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCompany(UpdateCargoCompanyDto updateCargoCompanyDto)
+        {
+            CargoCompany cargoCompany = new CargoCompany()
+            {
+                CargoCompanyId = updateCargoCompanyDto.CargoCompanyId,
+                CargoCompanyName = updateCargoCompanyDto.CargoCompanyName
+            };
+            return Ok("Kargo Şirketi Başarıyla güncellendi");
+        }
+
+
+    }
+}
