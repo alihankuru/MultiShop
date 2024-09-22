d987be55d99824731a634e247543c80f28bd3706
AK <ali.kuru6@hotmail.com>
2024-09-22 19:22:03 +0300

    Add Comment

---
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _context;
+
+        public CommentsController(CommentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _context.UserComments.ToList();    
+            return Ok(values);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _context.UserComments.Find(id);
+            _context.UserComments.Remove(value);
+            _context.SaveChanges();
+            return Ok("Yorum Başarıyla Silindi");
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _context.UserComments.Add(userComment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla eklendi");    
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var value = _context.UserComments.Find(id);
+            return Ok(value);
+
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _context.UserComments.Update(userComment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla eklendi");
+        }
+
+    }
+}
