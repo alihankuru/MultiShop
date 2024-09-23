cf556df7cf6f46ffd33ef5537b64285bc6b33167
AK <ali.kuru6@hotmail.com>
2024-09-24 00:26:35 +0300

    Add Comment

--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailReviewComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailReviewComponentPartial.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
 {
-    public class _ProductDetailReviewComponentPartial:ViewComponent
+    public class _ProductDetailReviewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductDetailReviewComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
         }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7101/api/Comments/CommentListByProductId?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+
+        }
+
+
     }
 }
--- Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -60,7 +60,7 @@ namespace MultiShop.Comment.Controllers
         [HttpGet("CommentListByProductId")]
         public IActionResult CommentListByProductId(string id)
         {
-            var value =_context.UserComments.Find(id);
+            var value =_context.UserComments.Where(x=>x.ProductId == id).ToList();
             return Ok(value);
         }
 
